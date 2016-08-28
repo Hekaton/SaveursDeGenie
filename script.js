06a8a7ca773ff110b3b98aba81f09d13a12b94ad
Dmitri K <kolytchev.d@gmail.com>
2016-08-28 00:46:42 -0400

    Adding a litle responsiveness

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(function(){
 	// Fade the top bar to black when scrolling beyond 30% of the first screen by adding .dark
+	var limit =  $('nav').height();
 	$(window).on('scroll', function(e){
 		var top = window.scrollY;
 
-		var isDark = top > $('#cover').first().height() * 0.3;
+		var isDark = $('#cover article').offset().top - top < limit;
 		$('nav').toggleClass('dark', isDark);
 
 		$('#cover').css("background-position", "0 " + (-top/2) + "px");
