cbdfebbabf2ce2d0c51ace2aa476daae572c87f4
Dmitri K <kolytchev.d@gmail.com>
2016-08-28 00:07:19 -0400

    Adding parallax BG because everybody loves that shit
    and I do too

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ $(function(){
 
 	// Fade the top bar to black when scrolling beyond 30% of the first screen by adding .dark
 	$(window).on('scroll', function(e){
-		var isDark = $("body").scrollTop() > $('section').first().height() * 0.3;
+		var top = $("body").scrollTop();
+		var isDark = top > $('#cover').first().height() * 0.3;
 		$('nav').toggleClass('dark', isDark);
+
+		$('#cover').css({"background-position-y": -top/2 + "px"})
 	}).trigger('scroll');
 
 })
