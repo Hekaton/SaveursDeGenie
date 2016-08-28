6cc544f3306061d1880be2295f6bc59e0c62acd8
Dmitri K <kolytchev.d@gmail.com>
2016-08-28 00:19:18 -0400

    Bugfix: parallax not working in edge or FF
    poor choice of property

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 $(function(){
-
-
 	// Fade the top bar to black when scrolling beyond 30% of the first screen by adding .dark
 	$(window).on('scroll', function(e){
-		var top = $("body").scrollTop();
+
 		var isDark = top > $('#cover').first().height() * 0.3;
 		$('nav').toggleClass('dark', isDark);
 
-		$('#cover').css({"background-position-y": -top/2 + "px"})
+		$('#cover').css("background-position", "0 " + (-top/2) + "px");
 	}).trigger('scroll');
 
 })
